68d3150e91c9e4905ba4d405b777b74e0e79dbef
Emilio Pujalka <epujalka@directo.com.ar>
2026-03-03 19:59:24 -0300

    fix: configurar variables de entorno en eas.json y corregir carga de fuentes para evitar crash en produccion

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
+import {
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_600SemiBold
+} from '@expo-google-fonts/inter';
+import {
+  Montserrat_200ExtraLight,
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+  Montserrat_900Black,
+  useFonts
+} from '@expo-google-fonts/montserrat';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useFonts } from 'expo-font';
 import * as Notifications from 'expo-notifications';
 import { Stack } from 'expo-router';
 import React from 'react';
@@ -22,15 +35,15 @@ const queryClient = new QueryClient();
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
-    Montserrat_200ExtraLight: require('../node_modules/@expo-google-fonts/montserrat/200ExtraLight/Montserrat_200ExtraLight.ttf'),
-    Montserrat_300Light: require('../node_modules/@expo-google-fonts/montserrat/300Light/Montserrat_300Light.ttf'),
-    Montserrat_400Regular: require('../node_modules/@expo-google-fonts/montserrat/400Regular/Montserrat_400Regular.ttf'),
-    Montserrat_700Bold: require('../node_modules/@expo-google-fonts/montserrat/700Bold/Montserrat_700Bold.ttf'),
-    Montserrat_800ExtraBold: require('../node_modules/@expo-google-fonts/montserrat/800ExtraBold/Montserrat_800ExtraBold.ttf'),
-    Montserrat_900Black: require('../node_modules/@expo-google-fonts/montserrat/900Black/Montserrat_900Black.ttf'),
-    Inter_300Light: require('../node_modules/@expo-google-fonts/inter/300Light/Inter_300Light.ttf'),
-    Inter_400Regular: require('../node_modules/@expo-google-fonts/inter/400Regular/Inter_400Regular.ttf'),
-    Inter_600SemiBold: require('../node_modules/@expo-google-fonts/inter/600SemiBold/Inter_600SemiBold.ttf'),
+    Montserrat_200ExtraLight,
+    Montserrat_300Light,
+    Montserrat_400Regular,
+    Montserrat_700Bold,
+    Montserrat_800ExtraBold,
+    Montserrat_900Black,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_600SemiBold,
   });
 
   if (!fontsLoaded) {
